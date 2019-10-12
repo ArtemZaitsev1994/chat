d098c64866761b3d8d9a9544a342e2a6dc3c36bb
Artem Zaitsev <artz1994@mail.ru>
2019-10-12 22:23:04 +0500

    asd

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ DEBUG = env.bool('DEBUG', default=False)
 SITE_HOST = env.str('HOST')
 SITE_PORT = env.int('PORT')
 SECRET_KEY = env.str('SECRET_KEY')
-MONGO_HOST = env.str('MONGO_HOST')
+MONGO_HOST = os.getenv('MONGODB_URI')
 MONGO_DB_NAME = env.str('MONGO_DB_NAME')
 
 MESSAGE_COLLECTION = 'messages'
