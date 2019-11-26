d4c09c5e9de4a155d4114f2af60c075e1bb63ece
Artem <artz1994@mail.ru>
2019-11-26 21:27:26 +0200

    golang

--- go_back/server.go
+++ go_back/server.go
@@ -1,22 +1,38 @@
 package main
 
 import (
-	"os"
+	// "os"
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
+    "golang.org/x/net/websocket"
 	// "github.com/gorilla/sessions"
 	"github.com/streadway/amqp"
-	"github.com/go-redis/redis"
+	// "github.com/go-redis/redis"
+    "strconv"
+    "strings"
+    "encoding/hex"
 
 )
 
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
-}
+type (
+	Msg struct {
+		clientKey string
+		text      string
+	}
+
+	NewClientEvent struct {
+		clientKey string
+		msgChan   chan *Msg
+	}
+)
+
+var (
+	clientRequests    = make(chan *NewClientEvent, 100)
+	clientDisconnects = make(chan string, 100)
+	messages          = make(chan *Msg, 100)
+)
 
 // var client = redis.Client
 
@@ -57,77 +73,165 @@ func CreateRabbitQueue(queue_name string) (amqp.Channel, string) {
 }
 
 
+func routeEvents() {
+	clients := make(map[string]chan *Msg)
 
+	for {
+		select {
+		case req := <-clientRequests:
+			clients[req.clientKey] = req.msgChan
+			fmt.Println("Websocket connected: " + req.clientKey)
+		case clientKey := <-clientDisconnects:
+			close(clients[clientKey])
+			delete(clients, clientKey)
+			fmt.Println("Websocket disconnected: " + clientKey)
+		case msg := <-messages:
+			for _, msgChan := range clients {
+				if len(msgChan) < cap(msgChan) {
+					msgChan <- msg
+				}
+			}
+		}
+	}
+}
 
-func main() {
-	// ExampleNewClient()
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+func WsChat(ws *websocket.Conn) {
+	lenBuf := make([]byte, 5)
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	// Output: PONG <nil>
-	// conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	FailOnError(err, "Failed to connect")
-	defer conn.Close()
+	msgChan := make(chan *Msg, 100)
+	clientKey := strings.SplitAfterN(ws.Request().URL.Path, "ws_chat/", 2)[1]
+	_, err := hex.DecodeString(clientKey)
+	FailOnError(err, "Wrong URL")
 
-	ch, err := conn.Channel()
-	FailOnError(err, "Fail to open a channel")
-	defer ch.Close()
+	clientRequests <- &NewClientEvent{clientKey, msgChan}
+	defer func() { clientDisconnects <- clientKey }()
 
-	_, err = ch.QueueDeclare(
-		"chat",
-		false,
-		true,
-		false,
-		false,
-		nil,
-	)
-	FailOnError(err, "Failed to declare a queue")
-	rabbit_chat_queue, q_name := ch, "chat"
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/go/ws_chat/{user_id}", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello World!")
-		vars := mux.Vars(r)
-    	user_id := vars["user_id"]
-    	fmt.Println(user_id)
+	go func() {
+		for msg := range msgChan {
+			ws.Write([]byte(msg.text))
+		}
+	}()
 
-		upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-		ws, err := upgrader.Upgrade(w, r, nil)
-		FailOnError(err, "Failed to upgrade to ws")
-		defer ws.Close()
-
-		err = client.SAdd("online", user_id, 0).Err()
-		FailOnError(err, "Failed to add ws to redis")
-
-		for {
-			_, p, err := ws.ReadMessage()
-			FailOnError(err, "Fail to read mess from ws")
-
-			fmt.Fprintln(os.Stdout, string(p))
-
-			// err = ws.WriteMessage(messageType, p)
-			// FailOnError(err, "Fail to send mess to ws")
-
-			err = rabbit_chat_queue.Publish(
-				"",     // exchange
-				q_name, // routing key
-				false,  // mandatory
-				false,  // immediate
-				amqp.Publishing {
-					ContentType: "text/plain",
-				    Body:        []byte(p),
-			})
-			FailOnError(err, "Failed to publish a message")
+	for {
+		_, err := ws.Read(lenBuf)
+		if err != nil {
+			fmt.Println("Error: ", err.Error())
+			return
 		}
 
-		err = client.SRem("online", user_id).Err()
-		FailOnError(err, "Failed to remove ws from redis")
-	})
+		length, _ := strconv.Atoi(strings.TrimSpace(string(lenBuf)))
+		if length > 65536 {
+			fmt.Println("Error: too big length: ", length)
+			return
+		}
+
+		if length <= 0 {
+			fmt.Println("Empty length: ", length)
+			return
+		}
+
+		buf := make([]byte, length)
+		_, err = ws.Read(buf)
+
+		if err != nil {
+			fmt.Println("Could not read ", length, " bytes: ", err.Error())
+			return
+		}
+
+		messages <- &Msg{clientKey, string(buf)}
+	}
+}
+
+func main() {
+	// -----------REDIS--------------
+	// client := redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379",
+	// 	Password: "", // no password set
+	// 	DB:       0,  // use default DB
+	// })
+
+	// pong, err := client.Ping().Result()
+	// fmt.Println(pong, err)
+	// Output: PONG <nil>
+	// -------------------------------
+
+	// -----------RABBIT--------------
+	// conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	// conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	// FailOnError(err, "Failed to connect")
+	// defer conn.Close()
+
+	// ch, err := conn.Channel()
+	// FailOnError(err, "Fail to open a channel")
+	// defer ch.Close()
+
+	// _, err = ch.QueueDeclare(
+	// 	"chat",
+	// 	false,
+	// 	true,
+	// 	false,
+	// 	false,
+	// 	nil,
+	// )
+	// FailOnError(err, "Failed to declare a queue")
+	// rabbit_chat_queue, q_name := ch, "chat"
+	// ----------------------------------
+
+
+
+
+
+	go routeEvents()
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.Handle("/go/ws_chat/{user_id}", websocket.Handler(WsChat))
+
+
+
+
+
+	// router.HandleFunc("/go/ws_chat/{user_id}", func (w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Println("Hello World!")
+	// 	vars := mux.Vars(r)
+ //    	user_id := vars["user_id"]
+ //    	fmt.Println(user_id)
+
+	// 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	// 	ws, err := upgrader.Upgrade(w, r, nil)
+	// 	FailOnError(err, "Failed to upgrade to ws")
+	// 	defer ws.Close()
+
+	// 	err = client.SAdd("online", user_id, 0).Err()
+	// 	FailOnError(err, "Failed to add ws to redis")
+
+	// 	for {
+	// 		_, p, err := ws.ReadMessage()
+	// 		FailOnError(err, "Fail to read mess from ws")
+
+	// 		fmt.Fprintln(os.Stdout, string(p))
+
+	// 		// err = ws.WriteMessage(messageType, p)
+	// 		// FailOnError(err, "Fail to send mess to ws")
+
+	// 		err = rabbit_chat_queue.Publish(
+	// 			"",     // exchange
+	// 			q_name, // routing key
+	// 			false,  // mandatory
+	// 			false,  // immediate
+	// 			amqp.Publishing {
+	// 				ContentType: "text/plain",
+	// 			    Body:        []byte(p),
+	// 		})
+	// 		FailOnError(err, "Failed to publish a message")
+	// 	}
+
+	// 	err = client.SRem("online", user_id).Err()
+	// 	FailOnError(err, "Failed to remove ws from redis")
+	// })
+
+
+
+
 
 	http.Handle("/", router)
 	// http.HandleFunc("/go/ws_common", func(w http.ResponseWriter, r *http.Request) {
